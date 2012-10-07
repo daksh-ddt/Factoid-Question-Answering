e2380412d6e04dc7a45151adec78f0d4fe8d6358
gavin <gavin@qx410.(none)>
2012-10-07 09:24:35 -0400

    added answer extraction for number

--- answerparser.py
+++ answerparser.py
@@ -7,8 +7,11 @@ Modified on Mon Oct 1 19:23:50 2012
 @author: gavin hackeling
 
 """
-
+import nltk.tokenize
 import alchemy
+
+from unidecode import unidecode
+
 
 def parseEntities(results, answerType, apphome):
     
@@ -26,10 +29,64 @@ def parseEntities(results, answerType, apphome):
         "religion" : ["ReligiousOrganization", "ReligiousOrder"],
         "sport" : ["Sport"],
         "dismed" : ["HealthCondition"]
-    }    
+    }
+    # for answertype 'other', need coarse label too
+    answerTypeOtherMap = {
+    
+    }
+    
+    answerTypePOS = [
+        'code',
+        'count',
+        'date',
+        'dist',
+        'money',
+        'ord',
+        'perc',
+        'period',
+        'speed',
+        'temp',
+        'volsize',
+        'weight'
+    ]
     
     if answerType in answerTypeNEMap:
         rankedAnswers = alchemy.parseEntities(results, answerType, apphome)
+    elif answerType in answerTypePOS:
+        rankedAnswers = []
+        for result in results:
+            result = unidecode(u'%s' % result)
+            resultTokens = nltk.tokenize.word_tokenize(result)
+            resultTags = nltk.pos_tag(resultTokens)
+            print '%s: %s' % (resultTokens, resultTags)
+            for tag in resultTags:
+                if tag[1] == 'CD':
+                    print '%s, %s' % (tag, tag[0])
+                    rankedAnswers.append([tag[0]])
+                #if tag[1] == 'CD':
+                 #   rankedAnswers.append(tag[0])
+        return rankedAnswers
     else:
         rankedAnswers = [["Sorry, I can't do that yet."]]
     return rankedAnswers
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
