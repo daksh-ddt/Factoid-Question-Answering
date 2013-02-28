646f67c43febb47c26b0e0179dfbb77e18585f6c
Gavin Hackeling <gavinhackeling@gmail.com>
2013-02-28 11:06:31 -0500

    added extractors

--- answer_extractor.py
+++ answer_extractor.py
@@ -4,8 +4,22 @@ Load the appropriate extraction module for the question type
 and extract candidate answers for verification.
 
 '''
+# desc_def is not functional
+from extractors.desc import desc_def
+# desc_manner is not functional
+from extractors.desc import desc_manner
+# desc_reason is not functional
+from extractors.desc import desc_reason
+# desc_hum is not functional
+from extractors.desc import desc_desc
+# hum_desc is not functional
+from extractors.hum import hum_desc
 from extractors.hum import hum_ind
 from extractors.hum import hum_ind_alchemy
+from extractors.hum import hum_gr
+from extractors.hum import hum_gr_alchemy
+# hum_title is not functional
+from extractors.hum import hum_title
 from extractors.num import num_count
 from extractors.loc import loc_planet
 from extractors.loc import loc_continent
@@ -23,17 +37,17 @@ class Answer_extractor:
 
     def __init__(self):
         hum_map_alchemy = {
-            'desc': 'hum_desc',
-            'gr': 'hum_gr',
+            'desc': hum_desc,
+            'gr': hum_gr_alchemy,
             'ind': hum_ind_alchemy,
-            'title': 'hum_title'
+            'title': hum_title,
         }
 
         hum_map = {
-            'desc': 'hum_desc',
-            'gr': 'hum_gr',
+            'desc': hum_desc,
+            'gr': hum_gr,
             'ind': hum_ind,
-            'title': 'hum_title'
+            'title': hum_title
         }
 
         loc_map = {
@@ -65,7 +79,7 @@ class Answer_extractor:
             'school': '',
             'sea': loc_sea,
             'source': '',
-            'state': loc_states,
+            'state': loc_state,
             'street': '',
             'vague': '',
             'web_address': ''
@@ -87,22 +101,41 @@ class Answer_extractor:
             'weight': 'num_weight'
         }
 
-        self.extraction_map = {
+        abbr_map = {
             'abbr': 'hum_map',
             'exp': 'num_map',
         }
 
-        abbr_map = {
-            'hum': 'hum_map',
-            'num': 'num_map',
-            'loc': 'loc_map',
+        desc_map = {
+            'def': desc_def,
+            'desc': desc_desc,
+            'manner': desc_manner,
+            'reason': desc_reason,
         }
 
-        desc_map = {
-            'def': 'hum_map',
-            'desc': 'num_map',
-            'manner': 'loc_map',
-            'reason': 'loc_map',
+        enty_map_alchemy = {
+            'animal': '',
+            'body': '',
+            'color': '',
+            'cremat': '',
+            'currency': '',
+            'dismed': '',
+            'event': '',
+            'food': '',
+            'instru': '',
+            'lang': '',
+            'letter': '',
+            'other': '',
+            'plant': '',
+            'product': '',
+            'religion': '',
+            'sport': '',
+            'substance': '',
+            'symbol': '',
+            'techmeth': '',
+            'termeq': '',
+            'veh': '',
+            'word': ''
         }
 
         enty_map = {
@@ -130,10 +163,19 @@ class Answer_extractor:
             'word': ''
         }
 
-        self.extraction_map_alchemy = {
+        self.extraction_map = {
             'abbr': abbr_map,
             'desc': desc_map,
             'enty': enty_map,
+            'hum': hum_map,
+            'loc': loc_map,
+            'num': num_map,
+        }
+
+        self.extraction_map_alchemy = {
+            'abbr': abbr_map,
+            'desc': desc_map,
+            'enty': enty_map_alchemy,
             'hum': hum_map_alchemy,
             'loc': loc_map,
             'num': num_map,
@@ -145,7 +187,7 @@ class Answer_extractor:
         if not use_alchemy:
             extractor = self.extraction_map[coarse][fine]
             ranked_answers = extractor.extract(
-                tokens, pos_tagged_documents)
+                tokens, pos_tagged_documents, ranked_docs)
         else:
             print('using alchemy api')
             extractor = self.extraction_map_alchemy[coarse][fine]
---
+++ extractors/hum/hum_desc.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Modified on Mon Sep 24 19:23:50 2012
+
+@author: gavin hackeling
+"""
+
+def extract(tokens, pos_tagged_documents, ranked_docs):
+    return "Sorry, I can't do that yet."
